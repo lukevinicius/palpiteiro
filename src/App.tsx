b31ee06ec3dc075ff4f09c32c69c06d09ddcb7e1
lucas vinicius <lucasviniciusaalves@gmail.com>
2022-03-15 14:11:36 -0300

    add: livechat

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import 'semantic-ui-css/semantic.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { LiveChatWidget } from '@livechat/widget-react';
+
 import Aos from 'aos';
 import { GlobalStyle } from './styles/global';
 import { Home } from './screens/Home';
@@ -8,11 +11,17 @@ import { Header } from './components/Header';
 
 function App() {
   Aos.init();
+
   return (
     <>
       <GlobalStyle />
       <Header />
       <Home />
+      <LiveChatWidget
+        license="13178247"
+        visibility="minimized"
+        group="Palpiteiro"
+      />
       <Footer />
     </>
   );
